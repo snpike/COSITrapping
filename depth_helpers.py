28e5c372eeaf4a6608e247f42e91f0d112525fc8
Sean Pike <seannpike@gmail.com>
2023-11-06 11:35:43 -0800

    Added the ability to pass multiple files to make_df_from_dat_CC

--- depth_helpers.py
+++ depth_helpers.py
@@ -111,7 +111,7 @@ def make_df_from_dat(file, e_min = 640., e_max = 672.):
     df = pd.DataFrame(rows,columns=col_names)
     return df
 
-def make_df_from_dat_CC(file, e_min = 640., e_max = 672.):
+def make_df_from_dat_CC(files, e_min = 640., e_max = 672.):
     
     # SH det  side  strip_num (starting at 1)  did strip trigger (0 or 1)  
            #timing raw AD counter units  corrected AD counter units  ADC  energy  ??  
@@ -120,51 +120,52 @@ def make_df_from_dat_CC(file, e_min = 640., e_max = 672.):
     rows = []
     col_names = ["ID","det","strip_p","energy_p","strip_n","energy_n"]
 
-    with open(file, "r") as f:
-        
-        ev_block = []
-        
-        for line in f:
-        #for each line, start a block of lines corresponding to an event
+    for file in files:
+        with open(file, "r") as f:
             
-            if line.startswith('SE'):
-            # If the accumulated block has 6 lines then it's a single-pixel event
+            ev_block = []
+            
+            for line in f:
+            #for each line, start a block of lines corresponding to an event
                 
-                if len(ev_block) == 8:
-                    if np.product(np.array([("StripPairingIncomplete" not in l) for l in ev_block])):
-                        
-                        ID = int(ev_block[0].split(" ")[-1].strip("\n"))
+                if line.startswith('SE'):
+                # If the accumulated block has 8 lines then it's a single-pixel event
+                    
+                    if len(ev_block) == 8:
+                        if np.product(np.array([("StripPairingIncomplete" not in l) for l in ev_block])):
+                            
+                            ID = int(ev_block[0].split(" ")[-1].strip("\n"))
 
-                        if len(ev_block[3].split(" ")) < 9:
-                               print(ev_block)
-                        if len(ev_block[4].split(" ")) < 9:
-                               print(ev_block)
+                            if len(ev_block[3].split(" ")) < 9:
+                                   print(ev_block)
+                            if len(ev_block[4].split(" ")) < 9:
+                                   print(ev_block)
 
-                        energy_p = float(ev_block[3].split(" ")[8])
-                        energy_n = float(ev_block[4].split(" ")[8])
+                            energy_p = float(ev_block[3].split(" ")[8])
+                            energy_n = float(ev_block[4].split(" ")[8])
 
-                        # select photopeak events
-                        if (energy_p < e_max and energy_p > e_min) and (energy_n < e_max and energy_n > e_min):
+                            # select photopeak events
+                            if (energy_p < e_max and energy_p > e_min) and (energy_n < e_max and energy_n > e_min):
 
-                            # save info from SH p line
-                            det = int(ev_block[3].split(" ")[1])
-                            strip_p = int(ev_block[3].split(" ")[3])
+                                # save info from SH p line
+                                det = int(ev_block[3].split(" ")[1])
+                                strip_p = int(ev_block[3].split(" ")[3])
 
-                            # save info from SH n line
-                            strip_n = int(ev_block[4].split(" ")[3])
+                                # save info from SH n line
+                                strip_n = int(ev_block[4].split(" ")[3])
 
-                            # save position [cm] info from HT line
-                            # x = float(ev_block[5].split(" ")[1])
-                            # y = float(ev_block[5].split(" ")[2])
-                            # z = float(ev_block[5].split(" ")[3])
+                                # save position [cm] info from HT line
+                                # x = float(ev_block[5].split(" ")[1])
+                                # y = float(ev_block[5].split(" ")[2])
+                                # z = float(ev_block[5].split(" ")[3])
 
-                            # save info to df
-                            columns = [ID,det,strip_p,energy_p,strip_n,energy_n]
-                            rows.append(columns)
-                ev_block = []
-                
-            else:
-                ev_block.append(line)
-    
+                                # save info to df
+                                columns = [ID,det,strip_p,energy_p,strip_n,energy_n]
+                                rows.append(columns)
+                    ev_block = []
+                    
+                else:
+                    ev_block.append(line)
+        
     df = pd.DataFrame(rows,columns=col_names)
     return df
